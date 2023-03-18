e4ab548272bfc624a6ac30eb54282edcd4514daf
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2023-03-19 00:59:49 +0100

    added dynamic growth
    Technically it is finished soooooooo

--- arena.h
+++ arena.h
@@ -6,79 +6,111 @@
 #include <string.h>
 #include <assert.h>
 
-#define ARENA_DEFAULT_CAPACITY (1024 * 16)
+#define REGION_DEFAULT_CAPACITY (1024 * 8)
 
 typedef unsigned int uint;
 
-// typedef struct Region Region;
-//
-// struct Region {
-//     Region* next;
-//     size_t mem_current;
-//     size_t mem_total;
-//     void* data;
-// };
+typedef struct Region Region;
 
-typedef struct Arena {
+struct Region {
+    Region* next;
     size_t mem_current;
     size_t mem_total;
     void* data;
+};
+
+Region region_new(size_t size);
+void region_free(Region* region);
+
+typedef struct Arena {
+    Region* begin;
+    Region* end;
 } Arena;
 
 typedef struct ArenaLocal {
     Arena* arena;
+    Region* region;
     size_t pos;
 } ArenaLocal;
 
 ArenaLocal arena_get_scratch(Arena* arena);
-void arena_release_scratch(ArenaLocal* arena);
-
-// create or destroy an "arena"
-// NOTE(fonsi): if not sure about what max_size to input, try with ARENA_DEFAULT_CAPACITY and work from there
-Arena arena_alloc(size_t max_size);
-void  arena_release(Arena* arena);
-
-// push some bytes into the "arena"
-// if size + current arena mem > arena mem total, it will realloc with double the current capacity
-void* arena_push(Arena* arena, size_t size);
-
-// some helpers
-void arena_clear(Arena* arena);
+void       arena_release_scratch(ArenaLocal* local);
+void       arena_free(Arena* arena);
+void*      arena_push(Arena* arena, size_t size);
+void       arena_clear(Arena* arena);
 
 #endif // ARENA_H
 
 #ifdef ARENA_IMPLEMENTATION
 
-Arena
-arena_alloc(size_t max_size)
+Region*
+region_new(size_t size)
 {
-    return (Arena) {
-        .mem_current = 0,
-        .mem_total = max_size,
-        .data = calloc(max_size, 1),
-    };
+    Region* result = calloc(sizeof(Region), 1);
+    assert(result);
+    result->data = calloc(size);
+    result->mem_total = size;
+    result->mem_current = 0;
+    result->next = NULL;
+    return result;
 }
 
 void
-arena_release(Arena* arena) {
-    assert(arena);
-    free(arena->data);
+region_free(Region* region)
+{
+    assert(region);
+    assert(region->data);
+    free(region->data);
+    region->data = NULL;
+    free(region);
+}
+
+void
+arena_free(Arena* arena) {
+    Region* region = arena->begin;
+    while(region) {
+        Region *r = region;
+        region = region->next;
+        region_free(r);
+    }
+    arena->begin = NULL;
+    arena->end = NULL;
 }
 
 void
 arena_clear(Arena* arena) {
-    memset(arena->data, 0, arena->mem_total);
-    arena->mem_current = 0;
+    Region* r = arena->begin;
+    while(r != NULL) {
+        r->mem_current = 0;
+        r = r->next;
+    }
+    arena->end = arena->begin;
 }
 
-// TODO(fonsi): rethink the growing/shrinking strategy because if reallocated all previous arena_push given to the code will be pointing to random memory
 void*
 arena_push(Arena* arena, size_t size)
 {
-    if(arena->mem_current + size >= arena->mem_total)
-        arena->data = realloc(arena->data, arena->mem_total * 2 + size);
-    void* result = arena->data + arena->mem_current;
-    arena->mem_current += size;
+    if(!arena->end) {
+        assert(!arena->begin);
+        size_t cap = REGION_DEFAULT_CAPACITY;
+        if(cap < size) cap = size;
+        arena->end = region_new(cap);
+        arena->begin = end;
+    }
+
+    while(arena->end->mem_current + size > arena->end->mem_total && arena->end->next != NULL)
+        arena->end = arena->end->next;
+
+    if(arena->end->mem_current + size > arena->end->mem_total) {
+        assert(arena->end->next == NULL);
+        size_t cap = REGION_DEFAULT_CAPACITY;
+        if(cap < size) cap = size;
+        arena->end->next = region_new(cap);
+        arena->end = arena->end->next;
+    }
+
+    void* result = arena->end->data + arena->end->mem_current;
+    arena->end->mem_current += size;
     return result;
 }
 
@@ -86,15 +118,28 @@ ArenaLocal
 arena_get_scratch(Arena* arena)
 {
     assert(arena);
-    return (ArenaLocal) {
-        .arena = arena,
-        .pos = arena->mem_current,
-    };
+    ArenaLocal result;
+    result.arena = arena;
+    result.region = arena->end;
+    result.pos = arena->end->mem_current;
+}
+
+static void
+recursive_region_release(Region* r)
+{
+    if(r == NULL) return;
+    recursive_region_release(r->next);
+    if(r->next == NULL) {
+        region_free(r);
+        r = NULL;
+    }
 }
 
 void
-arena_release_scratch(ArenaLocal* arena)
+arena_release_scratch(ArenaLocal* local)
 {
-    assert(arena);
-    arena.arena.mem_current = arena.pos;
+    assert(local);
+    local->arena->end = local->region;
+    local->arena->end->mem_current = local->pos;
+    recursive_region_release(local->arena->end->next);
 }
